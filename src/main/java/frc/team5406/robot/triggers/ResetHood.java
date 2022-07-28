3465df4816436cb5749cac63b2c0ea95bd06997f
Christian Femia <58710637+ChristianFemia@users.noreply.github.com>
2022-07-28 18:57:21 -0400

    Added 2022 Triggers

---
+++ src/main/java/frc/team5406/robot/triggers/ResetHood.java
@@ -0,0 +1,19 @@
+package frc.team5406.robot.triggers;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.team5406.robot.subsystems.shooter.HoodSubsystem;
+
+
+
+public class ResetHood extends Trigger {
+    HoodSubsystem hoodSubsystem;
+    public ResetHood (HoodSubsystem _hoodSubsystem){  
+        hoodSubsystem = _hoodSubsystem;
+    }
+
+    @Override
+    public boolean get() {
+      return !hoodSubsystem.hoodReset;
+    }
+  }
