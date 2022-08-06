b5eba3f2ca2162accde12e96453e448bb7ac190f
Christian Femia <58710637+ChristianFemia@users.noreply.github.com>
2022-08-06 19:19:40 -0400

    Added analog option for Intake.

--- src/main/java/frc/team5406/robot/commands/IntakeCommand.java
+++ src/main/java/frc/team5406/robot/commands/IntakeCommand.java
@@ -9,6 +9,7 @@ import frc.team5406.robot.subsystems.intake.IntakeSubsystem;
 public class IntakeCommand extends CommandBase {
     IntakeSubsystem intake;
     FeederSubsystem feeder;
+    DoubleSupplier analog;
 
     public IntakeCommand(IntakeSubsystem _intake, FeederSubsystem _feeder) {
         intake = _intake;
@@ -16,10 +17,17 @@ public class IntakeCommand extends CommandBase {
         addRequirements(intake, feeder);
     }
 
+    public IntakeCommand(IntakeSubsystem _intake, FeederSubsystem _feeder, DoubleSupplier _analog){
+        intake = _intake;
+        feeder = _feeder;
+        analog = _analog;
+        addRequirements(intake, feeder);
+    }
+
     @Override
     public void execute() {
         intake.intakeExtend();
-        feeder.setFeederBottomSpeed(1000); // FIXME
+        feeder.setFeederBottomSpeed(analog.getAsDouble() > 0.1 ? analog.getAsDouble() : 1000); // FIXME
     }
 
     @Override
@@ -30,5 +38,6 @@ public class IntakeCommand extends CommandBase {
     public void end(boolean interrupted) {
         System.out.println("Intake - End");
         intake.intakeRetract();
+        feeder.stopFeederBottom();
     }
 }
